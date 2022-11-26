1c95473db49578ddac27c2908f2cedd57dd25d94
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-26 19:38:14 +0100

    d

--- app/app.py
+++ app/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set
 #from tqdm import tqdm
 import preprocessor as prepro # text prepro
 
