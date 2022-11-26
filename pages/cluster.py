a40e13e03629ea0a2adff39e419149fa29ba33b1
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-26 20:45:55 +0100

    d

--- pages/cluster.py
+++ pages/cluster.py
@@ -5,3 +5,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import itertools
 import umap 
+
+
+df = pd.read_csv('/content/ADHD/datasets/KKI_phenotypic.csv')
+#dropping unrelevant columns
+df = df.drop(columns = ['Site', 'ADHD Measure', 'IQ Measure', 'Full2 IQ', 'QC_Rest_1', 'QC_Rest_2', 'QC_Rest_3', 'QC_Rest_4', 'QC_Anatomical_1', 'QC_Anatomical_2'])
