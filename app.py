6b0aded10a8f9b1ad4ce2b17e7e138c07047844e
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-27 14:04:57 +0100

    d

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import seaborn as sns; sns.set
+import plotly.express as px
 from tqdm import tqdm
 import preprocessor as prepro # text prepro
 
