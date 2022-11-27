e687ad05c3b0c5f3fba7c39a07013c6b061dc7e5
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-27 14:46:49 +0100

    d

--- app.py
+++ app.py
@@ -47,13 +47,17 @@ st.subheader("This app is made by Snorre and Mike")
 st.write("Before diving into this app, we highly recommend (if you don't know what it is already) diving into what ADHD is")
 st.write("If you don't know what ADHD is about we have linked a 2 minute video going through the basics of ADHD and what symptoms one might have")
 
+
+def shorten_vid_option(opt):
+    return opt.split("/")[-1]
+
 vidurl = st.selectbox(
     "Pick a video to play",
     (
         "https://www.youtube.com/watch?v=9TcNQkyxMj8&ab_channel=AmericanPsychiatricAssociation"
     ),
     0,
-    shorten_vid_option
+    shorten_vid_option,
 )
 
 st.video(vidurl)
