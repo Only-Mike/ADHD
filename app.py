1e1a69fc0e393a0ce4f907cb5d3d96daa86e0bfd
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-27 14:57:27 +0100

    d

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ import pyLDAvis.gensim_models as gensimvis
 st.title("ADHD🧠")
 st.subheader("This app is made by Snorre and Mike")
 st.write("Before diving into this app, we highly recommend (if you don't know what it is already) diving into what ADHD is")
-st.write("If you don't know what ADHD is about we have linked a 2 minute video going through the basics of ADHD and what symptoms one might have")
+st.write("If you don't know what ADHD is about we have linked some 2 minute videos going through the basics of ADHD and what symptoms one might have")
 
 def shorten_vid_option(opt):
     return opt.split("/")[-1]
@@ -60,81 +60,6 @@ vidurl = st.selectbox(
     shorten_vid_option,
 )
 st.video(vidurl, format="video/mp4", start_time=0 )
-
-#TEST
-VIDEO_EXTENSIONS = ["mp4", "ogv", "m4v", "webm"]
-
-# For sample video files, try the Internet Archive, or download a few samples here:
-# http://techslides.com/sample-webm-ogg-and-mp4-video-files-for-html5
-
-
-st.title("Video Widget Examples")
-
-st.header("Local video files")
-st.write(
-    "You can use st.video to play a locally-stored video by supplying it with a valid filesystem path."
-)
-
-
-def get_video_files_in_dir(directory):
-    out = []
-    for item in os.listdir(directory):
-        try:
-            name, ext = item.split(".")
-        except:
-            continue
-        if name and ext:
-            if ext in VIDEO_EXTENSIONS:
-                out.append(item)
-    return out
-
-
-avdir = os.path.expanduser("~")
-files = get_video_files_in_dir(avdir)
-
-if len(files) == 0:
-    st.write(
-        "Put some video files in your home directory (%s) to activate this player."
-        % avdir
-    )
-
-else:
-    filename = st.selectbox(
-        "Select a video file from your home directory (%s) to play" % avdir,
-        files,
-        0,
-    )
-
-    st.video(os.path.join(avdir, filename))
-st.header("Remote video playback")
-st.write("st.video allows a variety of HTML5 supported video links, including YouTube.")
-
-
-def shorten_vid_option(opt):
-    return opt.split("/")[-1]
-
-
-# A random sampling of videos found around the web.  We should replace
-# these with those sourced from the streamlit community if possible!
-vidurl = st.selectbox(
-    "Pick a video to play",
-    (
-        "https://youtu.be/_T8LGqJtuGc",
-        "https://www.youtube.com/watch?v=kmfC-i9WgH0",
-        "https://www.youtube.com/embed/sSn4e1lLVpA",
-        "http://www.rochikahn.com/video/videos/zapatillas.mp4",
-        "http://www.marmosetcare.com/video/in-the-wild/intro.webm",
-        "https://www.orthopedicone.com/u/home-vid-4.mp4",
-    ),
-    0,
-    shorten_vid_option,
-)
-
-st.video(vidurl)
-
-
-
-
 
 
 #"""In this section we clean the data"""
