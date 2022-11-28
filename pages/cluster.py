eebcabf10c7dc41e24b61ae04439ba88d907cf06
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-28 10:48:31 +0100

    d

--- pages/cluster.py
+++ pages/cluster.py
@@ -12,36 +12,43 @@ st.title("ADHD Clustering")
 st.subheader("This app is made by Snorre and Mike")
 st.write("ADHD \n Something about ADHD clustering")
 
-#read in dataset
-df = pd.read_csv('datasets/KKI_phenotypic.csv')
+@st.experimental_singleton
+def read_process_data():
+    #read in dataset
+    df = pd.read_csv('datasets/KKI_phenotypic.csv')
 
-#dropping unrelevant columns
-df = df.drop(columns = ['Site', 'ADHD Measure', 'IQ Measure', 'Full2 IQ', 'QC_Rest_1', 'QC_Rest_2', 'QC_Rest_3', 'QC_Rest_4', 'QC_Anatomical_1', 'QC_Anatomical_2'])
+    #dropping unrelevant columns
+    df = df.drop(columns = ['Site', 'ADHD Measure', 'IQ Measure', 'Full2 IQ', 'QC_Rest_1', 'QC_Rest_2', 'QC_Rest_3', 'QC_Rest_4', 'QC_Anatomical_1', 'QC_Anatomical_2'])
 
-#Round age for fewer unique values and making into integer
-df['Age'] = df['Age'].round(decimals = 0)
-df['Age'] = df['Age'].astype(int)
+    #Round age for fewer unique values and making into integer
+    df['Age'] = df['Age'].round(decimals = 0)
+    df['Age'] = df['Age'].astype(int)
 
-#Removes rows with -999 in the following columns
-df = df[df['Inattentive'] != -999]
-df = df[df['Hyper/Impulsive'] != -999]
-df = df[df['ADHD Index'] != -999]
+    #Removes rows with -999 in the following columns
+    df = df[df['Inattentive'] != -999]
+    df = df[df['Hyper/Impulsive'] != -999]
+    df = df[df['ADHD Index'] != -999]
 
-#Making none secondary dx into 0 and any secondary dx into 1
-df['Secondary Dx '].replace(('Simple phobia', 'Simple Phobia', 'simple phobias', 'ODD', 'Simple Phobia ', 'ODD; Phobia', 'Specific phobia', 'Phobia', 'social and simple phobia '), (1, 1, 1, 1, 1, 1, 1, 1, 1), inplace=True)
-df['Secondary Dx '] = df['Secondary Dx '].fillna(0).astype(int)
+    #Making none secondary dx into 0 and any secondary dx into 1
+    df['Secondary Dx '].replace(('Simple phobia', 'Simple Phobia', 'simple phobias', 'ODD', 'Simple Phobia ', 'ODD; Phobia', 'Specific phobia', 'Phobia', 'social and simple phobia '), (1, 1, 1, 1, 1, 1, 1, 1, 1), inplace=True)
+    df['Secondary Dx '] = df['Secondary Dx '].fillna(0).astype(int)
 
-#import Synthetic data creator SDV
-from sdv.tabular import GaussianCopula
-model = GaussianCopula()
-model.fit(df)
+    #import Synthetic data creator SDV
+    from sdv.tabular import GaussianCopula
+    model = GaussianCopula()
+    model.fit(df)
 
-#Creating the synthetic data
-synthetic_data = model.sample(500)
-synthetic_data.head()
+    #Creating the synthetic data
+    synthetic_data = model.sample(500)
+    synthetic_data.head()
 
-#Combining the two datasets
-df = pd.concat([synthetic_data, df])
+    #Combining the two datasets
+    df = pd.concat([synthetic_data, df])
+
+    return df
+
+
+
 
 #We will scale the data which requires the following tool
 scaler = StandardScaler()
