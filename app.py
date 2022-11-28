defd89b0ae8fc1175b45cbd9c1af019cf865066e
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-11-28 09:35:48 +0100

    d

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ st.write("If you have ADHD or ADD you can help us get more data by answering the
 st.caption("Link will come soon", unsafe_allow_html=True)
 
 #Tabs 
-tab1, tab2, tab3, tab4 = st.tabs(["Introduction", "Age and ADHD"])
+tab1, tab2, tab3, tab4 = st.tabs(["Introduction", "Age and ADHD", "Predictor - Do you have ADHD?", "Links to websites regarding ADHD"])
 
 with tab1:
     st.header("Introduction to this app")
