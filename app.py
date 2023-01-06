5dc5a9c6dd4e9000b224b8654afe8fb49b9b68d2
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2023-01-06 14:35:35 +0100

    d

--- app.py
+++ app.py
@@ -179,6 +179,7 @@ with tab4:
     st.subheader("Still WIP") 
     st.write("For the topic modeling, there is used a dataset which comes from a subreddit called ADHD.\
     It is used to see the 9 most discussed topics and the most relevant terms for each topic")
+    st.iframe("ldaADHD (1).html")
 
 with tab5:
     st.title("ADHD Clustering")
